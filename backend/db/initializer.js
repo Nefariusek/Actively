e0fab066898f64185b23386d876985c5410e44d4
Szymon <szymonpakula98@gmail.com>
2020-12-06 13:42:45 +0100

    initializer for all models

--- backend/db/initializer.js
+++ backend/db/initializer.js
@@ -33,65 +33,211 @@ const createModelBatch = async (model, data) => {
 
 const arrayWithCount = (count) => (func) => [...Array(count).keys()].map(func);
 
+const createItems = async (amount, models) => {
+  const itemData = arrayWithCount(amount)((x) => {
+    if (defaultItems[x] !== undefined) {
+      return defaultItems[x];
+    } else {
+      return {
+        name: 'Initializer Sword',
+        slot: 'Weapon',
+        description: 'Ordinary sword',
+        effect: 'Adds strength',
+        effect_value: 10,
+        price: 300,
+      };
+    }
+  });
+
+  return await createModelBatch(models.item, itemData);
+};
+
+const createInventories = async (amount, models, idCatalog) => {
+  const inventoryData = arrayWithCount(amount)((x) => {
+    return {
+      equippedItems: [],
+      backpack: [],
+      gold: 150,
+    };
+  });
+
+  return await createModelBatch(models.inventory, inventoryData);
+};
+
+const createStatistics = async (amount, models, idCatalog) => {
+  const statisticsData = arrayWithCount(amount)((x) => {
+    return {
+      quests_completed: 0,
+      streak: 3,
+    };
+  });
+
+  return await createModelBatch(models.statistics, statisticsData);
+};
+
+const createQuests = async (amount, models) => {
+  const questData = arrayWithCount(amount)((x) => {
+    if (defaultQuests[x] !== undefined) {
+      return defaultQuests[x];
+    } else {
+      return {
+        name: 'Initializer',
+        description: 'Run for 100 minutes.',
+        type: 'Activity',
+        duration: 24,
+        experience_reward: 300,
+        gold_reward: 200,
+        penalty: 100,
+      };
+    }
+  });
+
+  return await createModelBatch(models.quest, questData);
+};
+
+const createQuestbooks = async (amount, models, idCatalog) => {
+  const questbookData = arrayWithCount(amount)((x) => {
+    return {
+      quests: [],
+    };
+  });
+
+  return await createModelBatch(models.questbook, questbookData);
+};
+
+const createGuilds = async (amount, models, idCatalog) => {
+  const guildData = arrayWithCount(amount)((x) => {
+    return {
+      name: 'Initializers',
+      leader: null,
+      members: [],
+    };
+  });
+
+  return await createModelBatch(models.guild, guildData);
+};
+
+const createSocials = async (amount, models, idCatalog) => {
+  const socialData = arrayWithCount(amount)((x) => {
+    return {
+      main_guild: null,
+      guilds: [],
+    };
+  });
+
+  return await createModelBatch(models.social, socialData);
+};
+
+const createCharacters = async (
+  amount,
+  models,
+  inventoryCatalog,
+  statisticsCatalog,
+  questbookCatalog,
+  socialCatalog,
+) => {
+  const characterData = arrayWithCount(amount)((x) => {
+    return {
+      name: 'Administrator',
+      species: 'Warrior',
+      level: 3,
+      experience_points: 23,
+      experience_required: 100,
+      health: 44,
+      max_health: 50,
+      endurance: 100,
+      max_endurance: 100,
+      strength: 12,
+      dexterity: 6,
+      wisdom: 3,
+      inventory_id: inventoryCatalog[x] === undefined ? null : inventoryCatalog[x],
+      statistics_id: statisticsCatalog[x] === undefined ? null : statisticsCatalog[x],
+      questbook_id: questbookCatalog[x] === undefined ? null : questbookCatalog[x],
+      social_id: socialCatalog[x] === undefined ? null : socialCatalog[x],
+    };
+  });
+
+  return await createModelBatch(models.character, characterData);
+};
+
+const createUsers = async (amount, models, characterCatalog) => {
+  const userData = arrayWithCount(amount)((x) => {
+    return {
+      username: 'ActivelyAdmin',
+      email: `activelyadmin@mail.com`,
+      password: 'silne_haslo',
+      character_id: characterCatalog[x] === undefined ? null : characterCatalog[x],
+    };
+  });
+
+  return await createModelBatch(models.user, userData);
+};
+
 const itemsInitializer = async (models, idCatalog) => {
-  return await createItems();
+  return await createItems(defaultItems.length, models);
 };
 
 const inventoriesInitializer = async (models, idCatalog) => {
-  return await createInventories();
+  return await createInventories(1, models);
 };
 
 const statisticsInitializer = async (models, idCatalog) => {
-  return await createStatistics();
+  return await createStatistics(1, models);
 };
 
 const questsInitializer = async (models, idCatalog) => {
-  return await createQuests();
+  return await createQuests(defaultQuests.length, models);
 };
 
 const questbooksInitializer = async (models, idCatalog) => {
-  return await createQuestbooks();
+  return await createQuestbooks(1, models);
 };
 
 const guildsInitializer = async (models, idCatalog) => {
-  return await createGuilds();
+  return await createGuilds(1, models);
 };
 
 const socialsInitializer = async (models, idCatalog) => {
-  return await createSocials();
+  return await createSocials(1, models, idCatalog['guild']);
 };
 
 const charactersInitializer = async (models, idCatalog) => {
-  return await createCharacters();
+  return await createCharacters(
+    1,
+    models,
+    idCatalog['inventory'],
+    idCatalog['statistics'],
+    idCatalog['questbook'],
+    idCatalog['social'],
+  );
 };
 
 const usersInitializer = async (models, idCatalog) => {
-  return await createUsers();
+  return await createUsers(1, models, idCatalog['character']);
 };
 
 const defaultInitializers = new Map([
-  ['items', itemsInitializer],
-  ['inventories', inventoriesInitializer],
+  ['item', itemsInitializer],
+  ['inventory', inventoriesInitializer],
   ['statistics', statisticsInitializer],
-  ['quests', questsInitializer],
-  ['questbooks', questbooksInitializer],
-  ['guilds', guildsInitializer],
-  ['socials', socialsInitializer],
-  ['characters', charactersInitializer],
-  ['users', usersInitializer],
+  ['quest', questsInitializer],
+  ['questbook', questbooksInitializer],
+  ['guild', guildsInitializer],
+  ['social', socialsInitializer],
+  ['character', charactersInitializer],
+  ['user', usersInitializer],
 ]);
 
 const initializationOrder = [
-  'items',
-  'inventories',
-  'quest',
+  'item',
+  'inventory',
   'statistics',
-  'quests',
-  'questbooks',
-  'guilds',
-  'socials',
-  'characters',
-  'users',
+  'quest',
+  'questbook',
+  // 'guild',
+  'social',
+  'character',
+  'user',
 ];
 
 const initialize = async (models, initializers = defaultInitializers) => {
